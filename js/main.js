d9d7d93838097e28e8f9b7ed5b2df99dfdd9a6fe
khasanshinae <khasanshinae@oooinex.ru>
2021-10-04 08:54:13 +0300

    fixs

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
-const getIntRandom = (min, max)=>{
-  if (min<=max){
-    console.error('Первый параметр min, второй max');
-    return false;
+const getPositive = (number) => {
+  if(number < 0){
+    return number * -1;
   }
-  //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#получение_случайного_целого_числа_в_заданном_интервале_включительно
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return number;
 };
 
-const getFloatRandom = (min, max, order)=>{
-  if (min<=max){
-    console.error('Первый параметр min, второй max');
-    return false;
+const getIntRandom = (min, max) => {
+  const minPos = getPositive(min);
+  const maxPos = getPositive(max);
+  if (minPos >= maxPos){
+    alert('Первый параметр min, второй max');
+    return null;
   }
-  return +((Math.random() * (max - min + 1)) + min).toFixed(order);
+  //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#получение_случайного_целого_числа_в_заданном_интервале_включительно
+  const minCeil = Math.ceil(minPos);
+  const maxFloor = Math.floor(maxPos);
+  return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
+};
+
+const getFloatRandom = (min, max, order) => {
+  const minPos = getPositive(min);
+  const maxPos = getPositive(max);
+  const orderPos = getPositive(order);
+  if (minPos >= maxPos){
+    alert('Первый параметр min, второй max');
+    return null;
+  }
+  return +((Math.random() * (maxPos - minPos + 1)) + minPos).toFixed(orderPos);
 };
 
 getIntRandom(1, 5);
-getFloatRandom(1,5, 2);
+getFloatRandom(1, 5, 2);
